da2695ea6e77c2beda736177728a56eac499c39d
ProphetFlark <choper.paiz@gmail.com>
2023-05-25 22:51:29 -0600

    perro2

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import mongoose from "mongoose";
 import connectDatabase from "./config/MongoDb.js";
 import ImportData from "./DataImport.js";
@@ -11,6 +12,28 @@ import orderRouter from "./Routes/orderRoutes.js";
 dotenv.config();
 connectDatabase();
 const app = express();
+app.use(cors());
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "https://sercommerce.onrender.com/",
+    "http://sercommerce.onrender.com/m",
+    "https://hjcc-ommerce.netlify.app/",
+    "http://hjcc-ommerce.netlify.app/",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
 app.use(express.json());
 
 // API
